8d508cd4d75c6fd74146e6d39375229e89eae4a5
Enaium <32991121+Enaium@users.noreply.github.com>
2025-05-20 16:47:05 +0800

    Support copy execute SQL

---
+++ shared/main/kotlin/cn/enaium/jimmer/buddy/dialog/ExecuteSqlDialog.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2025 Enaium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.enaium.jimmer.buddy.dialog
+
+import com.intellij.openapi.editor.EditorFactory
+import com.intellij.openapi.fileTypes.PlainTextFileType
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.ui.EditorTextField
+import com.intellij.ui.components.JBLabel
+import org.jetbrains.kotlin.tools.projectWizard.wizard.ui.borderPanel
+import java.awt.Toolkit
+import java.awt.datatransfer.StringSelection
+import javax.swing.JComponent
+
+
+/**
+ * @author Enaium
+ */
+class ExecuteSqlDialog(val project: Project, val sql: String) : DialogWrapper(false) {
+    init {
+        title = "Execute SQL"
+        setSize(800, 600)
+        init()
+    }
+
+    override fun createCenterPanel(): JComponent {
+        return borderPanel {
+            addToTop(JBLabel("Recommend to enable Inline SQL Parameters in Jimmer"))
+            addToCenter(
+                EditorTextField(
+                    EditorFactory.getInstance().createDocument(sql),
+                    project,
+                    PlainTextFileType.INSTANCE,
+                    false
+                ).also {
+                    it.setOneLineMode(false)
+                })
+            addToBottom(JBLabel("Ok is copy to clipboard, Cancel is close"))
+        }
+    }
+
+    override fun doOKAction() {
+        Toolkit.getDefaultToolkit().systemClipboard.setContents(StringSelection(sql), null)
+        super.doOKAction()
+    }
+}
---
+++ shared/main/kotlin/cn/enaium/jimmer/buddy/extensions/console/ExecuteSqlFilter.kt
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2025 Enaium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.enaium.jimmer.buddy.extensions.console
+
+import cn.enaium.jimmer.buddy.JimmerBuddy
+import cn.enaium.jimmer.buddy.dialog.ExecuteSqlDialog
+import cn.enaium.jimmer.buddy.utility.invokeLater
+import com.intellij.codeInsight.hints.presentation.InputHandler
+import com.intellij.execution.filters.ConsoleFilterProvider
+import com.intellij.execution.filters.Filter
+import com.intellij.execution.impl.InlayProvider
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.EditorCustomElementRenderer
+import com.intellij.openapi.editor.Inlay
+import com.intellij.openapi.editor.impl.EditorImpl
+import com.intellij.openapi.editor.markup.TextAttributes
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.TextRange
+import com.jetbrains.rd.util.printlnError
+import java.awt.Cursor
+import java.awt.Graphics
+import java.awt.Point
+import java.awt.Rectangle
+import java.awt.event.MouseEvent
+
+/**
+ * @author Enaium
+ */
+class ExecuteSqlFilter : Filter, ConsoleFilterProvider {
+
+    val sql = "SQL: "
+    val start = "Execute SQL===>"
+    val end = "<===Execute SQL"
+
+    override fun applyFilter(line: String, offset: Int): Filter.Result? {
+        if (line.contains(start)) {
+            return Filter.Result(
+                listOf(
+                    Result(offset),
+                    Filter.ResultItem(0, 0, null)
+                )
+            )
+        }
+        return null
+    }
+
+    override fun getDefaultFilters(project: Project): Array<out Filter> {
+        return arrayOf(this)
+    }
+
+    inner class Result(val offset: Int) : Filter.Result(offset, offset, null), InlayProvider {
+        override fun createInlayRenderer(editor: Editor): EditorCustomElementRenderer {
+            return Renderer(editor, offset)
+        }
+    }
+
+    inner class Renderer(val editor: Editor, val offset: Int) : EditorCustomElementRenderer, InputHandler {
+
+        var hovered = false
+
+        override fun calcWidthInPixels(inlay: Inlay<*>): Int {
+            return 24
+        }
+
+        private fun getExecuteSql(): String {
+            val sqlOffset = editor.document.text.indexOf(sql, offset)
+            val endOffset = editor.document.text.indexOf(end, offset)
+            val text = editor.document.getText(TextRange(sqlOffset, endOffset))
+            return text.lines().let { it.subList(0, it.size - 3) }.joinToString("\n").substring(5)
+        }
+
+        override fun mouseClicked(event: MouseEvent, translated: Point) {
+            invokeLater {
+                editor.project?.also { project ->
+                    ExecuteSqlDialog(project, getExecuteSql()).show()
+                }
+            }
+        }
+
+        override fun mouseMoved(event: MouseEvent, translated: Point) {
+            hovered = true
+            (editor as EditorImpl).setCustomCursor(this, Cursor.getPredefinedCursor(Cursor.HAND_CURSOR))
+        }
+
+        override fun mouseExited() {
+            hovered = false
+            (editor as EditorImpl).setCustomCursor(this, null)
+        }
+
+        override fun paint(inlay: Inlay<*>, g: Graphics, targetRegion: Rectangle, textAttributes: TextAttributes) {
+            JimmerBuddy.Icons.LOGO_NORMAL.paintIcon(inlay.editor.component, g, targetRegion.x, targetRegion.y)
+        }
+    }
+}
