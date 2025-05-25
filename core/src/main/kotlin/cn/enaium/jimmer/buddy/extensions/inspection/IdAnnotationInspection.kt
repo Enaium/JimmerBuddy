fd56f9abf2f86d524ac98d76cf1d5a9949a49e9d
Enaium <wyy-666@hotmail.com>
2025-05-25 20:46:38 +0800

    Support id and super type inspection

---
+++ core/src/main/kotlin/cn/enaium/jimmer/buddy/extensions/inspection/IdAnnotationInspection.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2025 Enaium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.enaium.jimmer.buddy.extensions.inspection
+
+import cn.enaium.jimmer.buddy.utility.annotations
+import cn.enaium.jimmer.buddy.utility.getAllProperties
+import cn.enaium.jimmer.buddy.utility.isImmutable
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiElement
+import org.babyfish.jimmer.sql.Entity
+import org.babyfish.jimmer.sql.Id
+import org.jetbrains.kotlin.psi.KtClass
+
+/**
+ * @author Enaium
+ */
+class IdAnnotationInspection : AbstractLocalInspectionTool() {
+    override fun visit(
+        element: PsiElement,
+        holder: ProblemsHolder,
+        isOnTheFly: Boolean
+    ) {
+        val descriptionTemplate = "Entity does not have an id property"
+        if (element is PsiClass && element.isImmutable() && element.annotations
+                .any { it.qualifiedName == Entity::class.qualifiedName }
+        ) {
+            if (!element.allMethods.any { method -> method.annotations.any { it.qualifiedName == Id::class.qualifiedName } }) {
+                holder.registerProblem(element.nameIdentifier ?: return, descriptionTemplate)
+            }
+        } else if (element is KtClass && element.isImmutable() && element.annotations()
+                .any { it.fqName == Entity::class.qualifiedName }
+        ) {
+            if (!element.getAllProperties()
+                    .any { property -> property.annotations().any { it.fqName == Id::class.qualifiedName } }
+            ) {
+                holder.registerProblem(element.nameIdentifier ?: return, descriptionTemplate)
+            }
+        }
+    }
+}
---
+++ core/src/main/kotlin/cn/enaium/jimmer/buddy/extensions/inspection/SuperTypeInspection.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2025 Enaium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.enaium.jimmer.buddy.extensions.inspection
+
+import cn.enaium.jimmer.buddy.utility.annotations
+import cn.enaium.jimmer.buddy.utility.type
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiJavaCodeReferenceElement
+import com.intellij.psi.PsiReferenceList
+import org.babyfish.jimmer.sql.MappedSuperclass
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.psiUtil.getChildOfType
+import org.jetbrains.kotlin.psi.psiUtil.getChildrenOfType
+
+/**
+ * @author Enaium
+ */
+class SuperTypeInspection : AbstractLocalInspectionTool() {
+    override fun visit(
+        element: PsiElement,
+        holder: ProblemsHolder,
+        isOnTheFly: Boolean
+    ) {
+        val descriptionTemplate = "Super type must be annotated with @MappedSuperclass"
+        if (element is PsiClass) {
+            element.getChildrenOfType<PsiReferenceList>().forEach { psiReferenceList ->
+                psiReferenceList.getChildrenOfType<PsiJavaCodeReferenceElement>()
+                    .forEach { psiJavaCodeReferenceElement ->
+                        if ((psiJavaCodeReferenceElement.resolve() as? PsiClass)?.annotations?.any { annotation -> annotation.qualifiedName == MappedSuperclass::class.qualifiedName } == false) {
+                            holder.registerProblem(psiJavaCodeReferenceElement, descriptionTemplate)
+                        }
+                    }
+            }
+        } else if (element is KtClass) {
+            element.superTypeListEntries.forEach { superTypeListEntry ->
+                superTypeListEntry.typeReference?.type()?.ktClass?.annotations()
+                    ?.find { annotation -> annotation.fqName == MappedSuperclass::class.qualifiedName }
+                    ?: run {
+                        holder.registerProblem(superTypeListEntry, descriptionTemplate)
+                    }
+            }
+        }
+    }
+}
