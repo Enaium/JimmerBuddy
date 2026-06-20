592cb35f5ac354239052a0cfd5f6447bdd867620
Enaium <32991121+Enaium@users.noreply.github.com>
2026-06-20 14:06:15 +0800

    Support navigate to dto prop (#176)

--- core/src/main/kotlin/cn/enaium/jimmer/buddy/action/GoToGeneratedByFile.kt
+++ core/src/main/kotlin/cn/enaium/jimmer/buddy/action/GoToGeneratedByFile.kt
@@ -16,23 +16,34 @@
 
 package cn.enaium.jimmer.buddy.action
 
+import cn.enaium.jimmer.buddy.extensions.dto.DtoLanguage.findChild
+import cn.enaium.jimmer.buddy.extensions.dto.psi.DtoPsiDtoType
+import cn.enaium.jimmer.buddy.extensions.dto.psi.DtoPsiFile
+import cn.enaium.jimmer.buddy.extensions.dto.psi.DtoPsiRoot
 import cn.enaium.jimmer.buddy.utility.classLiteral
 import cn.enaium.jimmer.buddy.utility.findProjectDir
+import cn.enaium.jimmer.buddy.utility.firstCharLowercase
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.pom.Navigatable
 import com.intellij.psi.JavaPsiFacade
+import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiManager
 import com.intellij.psi.PsiMember
+import com.intellij.psi.util.findParentOfType
 import org.babyfish.jimmer.internal.GeneratedBy
 import org.jetbrains.kotlin.idea.base.util.allScope
 import org.jetbrains.kotlin.idea.core.util.toVirtualFile
 import org.jetbrains.kotlin.idea.stubindex.KotlinFullClassNameIndex
+import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtParameter
 import org.jetbrains.kotlin.psi.psiUtil.containingClass
 import org.jetbrains.uast.UClass
 import org.jetbrains.uast.toUElementOfType
+import java.util.*
 
 /**
  * @author Enaium
@@ -41,20 +52,33 @@ class GoToGeneratedByFile : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val dataContext = e.dataContext
         dataContext.getData(CommonDataKeys.PSI_ELEMENT)?.also { psiElement ->
-
             val element = when (psiElement) {
                 is PsiMember -> {
-                    psiElement.containingClass
+                    var containingClass = psiElement.containingClass
+                    while (containingClass?.findParentOfType<PsiClass>() != null) {
+                        containingClass?.findParentOfType<PsiClass>()?.also {
+                            containingClass = it
+                        }
+                    }
+                    containingClass
                 }
 
                 is KtElement -> {
-                    psiElement.containingClass()
+                    var containingClass = psiElement.containingClass()
+                    while (containingClass?.findParentOfType<KtClass>() != null) {
+                        containingClass?.findParentOfType<KtClass>()?.also {
+                            containingClass = it
+                        }
+                    }
+                    containingClass
                 }
 
                 else -> {
                     null
                 }
             }
+
+
 
             ((element ?: psiElement).toUElementOfType<UClass>())?.also { uClass ->
                 val generatedBy =
@@ -66,7 +90,65 @@ class GoToGeneratedByFile : AnAction() {
                         PsiManager.getInstance(psiElement.project)
                             .findFile(dtoFile.toFile().toVirtualFile() ?: return@also)
                             ?: return@also
-                    psiFile.navigate(true)
+
+                    if (psiFile is DtoPsiFile) {
+                        val dtoType =
+                            psiFile.findChild<DtoPsiRoot>("/dto")?.dtoTypes?.find { it.name?.text == uClass.name }
+
+                        when (psiElement) {
+                            is PsiClass, is KtClass -> {
+                                (dtoType?.name as? Navigatable)?.navigate(true)
+                            }
+
+                            is PsiMember -> {
+                                var containingClass = psiElement.containingClass
+                                val trace = mutableListOf(psiElement.name?.let {
+                                    if (it.startsWith("get") || it.startsWith("set")) {
+                                        it.substring(3).firstCharLowercase()
+                                    } else {
+                                        it
+                                    }
+                                } ?: return@also)
+                                val prefix = "TargetOf_"
+                                val className = containingClass?.name
+                                if (className?.startsWith(prefix) == true) {
+                                    trace.add(className.substring(prefix.length))
+                                    while (containingClass?.findParentOfType<PsiClass>() != null) {
+                                        containingClass.findParentOfType<PsiClass>()?.also {
+                                            val className = it.name
+                                            if (className?.startsWith(prefix) == true) {
+                                                trace.add(className.substring(prefix.length))
+                                            }
+                                            containingClass = it
+                                        }
+                                    }
+                                }
+                                dtoType?.navigate(trace.reversed())
+                            }
+
+                            is KtParameter -> {
+                                var containingClass = psiElement.containingClass()
+                                val trace = mutableListOf(psiElement.name ?: return@also)
+                                val prefix = "TargetOf_"
+                                val className = containingClass?.name
+                                if (className?.startsWith(prefix) == true) {
+                                    trace.add(className.substring(prefix.length))
+                                    while (containingClass?.findParentOfType<KtClass>() != null) {
+                                        containingClass.findParentOfType<KtClass>()?.also {
+                                            val className = it.name
+                                            if (className?.startsWith(prefix) == true) {
+                                                trace.add(className.substring(prefix.length))
+                                            }
+                                            containingClass = it
+                                        }
+                                    }
+                                }
+                                dtoType?.navigate(trace.reversed())
+                            }
+                        }
+                    } else {
+                        psiFile.navigate(true)
+                    }
                 }
                 generatedBy.findAttributeValue("type")?.classLiteral()?.also { qualifiedName ->
                     (JavaPsiFacade.getInstance(psiElement.project)
@@ -76,6 +158,20 @@ class GoToGeneratedByFile : AnAction() {
                     }
                 }
             }
+        }
+    }
+
+    fun DtoPsiDtoType.navigate(trace: List<String>) {
+        val trace = ArrayDeque(trace)
+        var body = body
+        while (body != null) {
+            trace.poll().also { poll ->
+                val find = body.explicitProps.find {
+                    body = it.positiveProp?.body
+                    it.positiveProp?.prop?.text == poll
+                }
+                (find?.positiveProp?.prop as? Navigatable)?.navigate(true)
+            } ?: break
         }
     }
 
