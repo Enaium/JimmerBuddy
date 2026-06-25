f99b29e3aad6095bcb05e998fe1bb58d2bb81eed
zjarlin <zjarlin@outlook.com>
2026-06-25 19:29:47 +0800

    feat: add association annotation quick fixes

--- core/src/main/kotlin/cn/enaium/jimmer/buddy/extensions/inspection/AssociationAnnotationInspection.kt
+++ core/src/main/kotlin/cn/enaium/jimmer/buddy/extensions/inspection/AssociationAnnotationInspection.kt
@@ -17,17 +17,38 @@
 package cn.enaium.jimmer.buddy.extensions.inspection
 
 import cn.enaium.jimmer.buddy.utility.*
+import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiSubstitutor
+import com.intellij.psi.codeStyle.JavaCodeStyleManager
 import com.intellij.psi.util.PsiUtil
+import org.babyfish.jimmer.sql.ManyToMany
+import org.babyfish.jimmer.sql.ManyToOne
+import org.babyfish.jimmer.sql.OneToMany
+import org.babyfish.jimmer.sql.OneToOne
+import org.jetbrains.kotlin.idea.core.ShortenReferences
 import org.jetbrains.kotlin.psi.KtProperty
+import org.jetbrains.kotlin.psi.KtPsiFactory
 
 /**
  * @author Enaium
  */
 class AssociationAnnotationInspection : AbstractLocalInspectionTool() {
+
+    private val toOneQuickFixes = arrayOf(
+        AddAssociationAnnotationQuickFix(OneToOne::class.qualifiedName!!, OneToOne::class.simpleName!!),
+        AddAssociationAnnotationQuickFix(ManyToOne::class.qualifiedName!!, ManyToOne::class.simpleName!!)
+    )
+
+    private val toManyQuickFixes = arrayOf(
+        AddAssociationAnnotationQuickFix(OneToMany::class.qualifiedName!!, OneToMany::class.simpleName!!),
+        AddAssociationAnnotationQuickFix(ManyToMany::class.qualifiedName!!, ManyToMany::class.simpleName!!)
+    )
+
     override fun visit(
         element: PsiElement,
         holder: ProblemsHolder,
@@ -58,13 +79,13 @@ class AssociationAnnotationInspection : AbstractLocalInspectionTool() {
                         ?.element?.isEntity() == true && !element.hasToManyAnnotation()
                     && !element.isComputed() && !element.hasManyToManyViewAnnotation()
                 ) {
-                    holder.registerProblem(element, noToManyProblem)
+                    holder.registerProblem(element, noToManyProblem, *toManyQuickFixes)
                 }
             } else {
                 if (element.hasToManyAnnotation()) {
                     holder.registerProblem(element, toOneProblem)
                 } else if (returnPsiClass?.element?.isEntity() == true && !element.hasToOneAnnotation() && !element.isComputed()) {
-                    holder.registerProblem(element, noToOneProblem)
+                    holder.registerProblem(element, noToOneProblem, *toOneQuickFixes)
                 }
             }
         } else if (element is KtProperty) {
@@ -78,14 +99,47 @@ class AssociationAnnotationInspection : AbstractLocalInspectionTool() {
                 if (element.hasToOneAnnotation()) {
                     holder.registerProblem(element, toManyProblem)
                 } else if (typeReference.firstArgType()?.ktClass?.isEntity() == true && !element.hasToManyAnnotation() && !element.isComputed() && !element.hasManyToManyViewAnnotation()) {
-                    holder.registerProblem(element, noToManyProblem)
+                    holder.registerProblem(element, noToManyProblem, *toManyQuickFixes)
                 }
             } else {
                 if (element.hasToManyAnnotation()) {
                     holder.registerProblem(element, toOneProblem)
                 } else if (typeReference?.ktClass?.isEntity() == true && !element.hasToOneAnnotation() && !element.isComputed()) {
-                    holder.registerProblem(element, noToOneProblem)
+                    holder.registerProblem(element, noToOneProblem, *toOneQuickFixes)
                 }
+            }
+        }
+    }
+}
+
+private class AddAssociationAnnotationQuickFix(
+    private val annotationQualifiedName: String,
+    private val annotationSimpleName: String
+) : LocalQuickFix {
+    override fun getName(): String {
+        return I18n.message("intention.annotation.association.add", annotationSimpleName)
+    }
+
+    override fun getFamilyName(): String {
+        return name
+    }
+
+    override fun applyFix(
+        project: Project,
+        descriptor: ProblemDescriptor
+    ) {
+        val element = descriptor.psiElement
+        if (element is PsiMethod) {
+            if (element.modifierList.annotations.none { it.qualifiedName == annotationQualifiedName }) {
+                val annotation = element.modifierList.addAnnotation(annotationQualifiedName)
+                JavaCodeStyleManager.getInstance(project).shortenClassReferences(annotation)
+            }
+        } else if (element is KtProperty) {
+            if (element.annotations().none { it.fqName == annotationQualifiedName }) {
+                val annotation = element.addAnnotationEntry(
+                    KtPsiFactory(project).createAnnotationEntry("@$annotationQualifiedName")
+                )
+                ShortenReferences.DEFAULT.process(annotation)
             }
         }
     }
