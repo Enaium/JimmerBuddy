e1277f5def6433e1f63f3e7ae3d0e7b2e89ebe84
Enaium <wyy-666@hotmail.com>
2026-06-27 01:28:16 +0800

    Support annotation parameters completion

---
+++ core/src/main/kotlin/cn/enaium/jimmer/buddy/extensions/dto/psi/impl/DtoPsiAnnotationNamedArgumentImpl.kt
@@ -0,0 +1,11 @@
+package cn.enaium.jimmer.buddy.extensions.dto.psi.impl
+
+import cn.enaium.jimmer.buddy.extensions.dto.psi.DtoPsiAnnotationArguments
+import cn.enaium.jimmer.buddy.extensions.dto.psi.DtoPsiAnnotationNamedArgument
+import com.intellij.lang.ASTNode
+import org.antlr.intellij.adaptor.psi.ANTLRPsiNode
+
+class DtoPsiAnnotationNamedArgumentImpl(node: ASTNode) : ANTLRPsiNode(node), DtoPsiAnnotationArguments,
+    DtoPsiAnnotationNamedArgument {
+
+}
